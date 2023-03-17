646f835eb6fcde3b1805710cb05cedb94c002441
mortaje123 <dikaramadya35@gmail.com>
2023-03-17 17:45:30 +0700

    latihan web II

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya dika ramadya  ,
+        saya tinggal di daerah Depok ngekos,
+        olahraga yang saya sukai push up di malam hari";
+
+    }
+}
